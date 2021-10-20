9c3ac6f69dbd246a64cca8eae86fd92f79c8f333
danchik61 <dan61ik@gmail.com>
2021-10-20 12:19:01 +0600

    initial commit.

---
+++ git_commands.py
@@ -0,0 +1,16 @@
+git init - .git - vse neobhodimye file repozitoriy git_hub
+git remote add name url 
+git pull stagivaem izmeneniya s vetki 
+git pull origin master
+git status - status file projecta
+git add - dobavlyaet  v  index dlya dalneyshego komita v github
+or
+git . 
+
+git commit -m  -  dobavlyaet v git s indexa  
+
+git branch name branch
+git checkout name  branch
+git push name branch vetka
+git push origin master 
+git  reset filename
